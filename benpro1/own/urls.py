8d9cc596b6e6e22d60b92d945effa41708c99194
bekam <benkamau2040@gmail.com>
2025-11-14 22:25:54 +0300

    django two_V

--- benpro1/own/urls.py
+++ benpro1/own/urls.py
@@ -4,7 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home_page'),
-    path('add/', views.add_Task, name='add_task')
+    path('add/', views.add_Task, name='add_task'),
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+    path('delete/<int:pk>/', views.delete, name='delete'),
+    path('edit/<int:pk>/', views.edit, name='edit')
+
 ]
 
 
--- benpro1/own/views.py
+++ benpro1/own/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Task
 
 
 def home(request):
     Tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    completed_task = Task.objects.filter(is_completed=True)
+
     context = {
-        'tasks': Tasks
+        'tasks': Tasks,
+        'compl_task': completed_task
     }
     return render(request, 'HTML/home.html', context)
 
@@ -15,6 +18,43 @@ def add_Task(request):
     task = request.POST['task']
     Task.objects.create(task=task)
     return redirect('home_page')
+
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, id=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home_page')
+
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, id=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home_page')
+
+
+def delete(request, pk):
+    task = get_object_or_404(Task, id=pk)
+    task.delete()
+    return redirect('home_page')
+
+
+def edit(request, pk):
+    get_task = get_object_or_404(Task, id=pk)
+
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home_page')
+    else:
+        context = {
+            'get_task': get_task
+        }
+
+        return render(request, 'HTML/edit.html', context)
+
 
 # def home(request):
 #     tasks = Task.objects.filter(is_completed=False).order_by('-created_at')
